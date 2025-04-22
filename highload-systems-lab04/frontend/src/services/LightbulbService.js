7ff659c7e3a8536520b30c8fbe2265b4833e305b
Maksym Nesterov <maksym.nesterov@betterme.world>
2025-04-22 21:23:54 +0300

    update service to use promises

--- highload-systems-lab04/frontend/src/services/LightbulbService.js
+++ highload-systems-lab04/frontend/src/services/LightbulbService.js
@@ -1,4 +1,4 @@
-import { LightbulbServiceClient } from '../proto/lightbulb_grpc_web_pb';
+import { LightbulbServicePromiseClient } from '../proto/lightbulb_grpc_web_pb';
 import {
   GetStateRequest,
   SetPowerRequest,
@@ -7,87 +7,39 @@ import {
 } from '../proto/lightbulb_pb';
 
 // Create a client instance
-const client = new LightbulbServiceClient('http://localhost:8080');
+const client = new LightbulbServicePromiseClient('http://localhost:8080');
+
+const responseToStateObject = (response) => {
+  return {
+    isOn: response.getIsOn(),
+    brightness: response.getBrightness(),
+    colorTemperature: response.getColorTemperature()
+  };
+};
 
 // Get the current state of the lightbulb
-export const getState = () => {
-  return new Promise((resolve, reject) => {
-    const request = new GetStateRequest();
-    client.getState(request, {}, (err, response) => {
-      if (err) {
-        console.error('Error getting lightbulb state:', err);
-        reject(err);
-      } else {
-        const state = {
-          isOn: response.getIsOn(),
-          brightness: response.getBrightness(),
-          colorTemperature: response.getColorTemperature()
-        };
-        resolve(state);
-      }
-    });
-  });
+export const getState = async () => {
+  const response = await client.getState(new GetStateRequest());
+  return responseToStateObject(response);
 };
 
 // Set the power state of the lightbulb
-export const setPower = (isOn) => {
-  return new Promise((resolve, reject) => {
-    const request = new SetPowerRequest();
-    request.setIsOn(isOn);
-    client.setPower(request, {}, (err, response) => {
-      if (err) {
-        console.error('Error setting lightbulb power:', err);
-        reject(err);
-      } else {
-        const state = {
-          isOn: response.getIsOn(),
-          brightness: response.getBrightness(),
-          colorTemperature: response.getColorTemperature()
-        };
-        resolve(state);
-      }
-    });
-  });
+export const setPower = async (isOn) => {
+  const request = new SetPowerRequest().setIsOn(isOn);
+  const response = await client.setPower(request);
+  return responseToStateObject(response);
 };
 
 // Set the brightness of the lightbulb
-export const setBrightness = (brightness) => {
-  return new Promise((resolve, reject) => {
-    const request = new SetBrightnessRequest();
-    request.setBrightness(brightness);
-    client.setBrightness(request, {}, (err, response) => {
-      if (err) {
-        console.error('Error setting lightbulb brightness:', err);
-        reject(err);
-      } else {
-        const state = {
-          isOn: response.getIsOn(),
-          brightness: response.getBrightness(),
-          colorTemperature: response.getColorTemperature()
-        };
-        resolve(state);
-      }
-    });
-  });
+export const setBrightness = async (brightness) => {
+  const request = new SetBrightnessRequest().setBrightness(brightness);
+  const response = await client.setBrightness(request);
+  return responseToStateObject(response);
 };
 
 // Set the color temperature of the lightbulb
-export const setColorTemperature = (temperature) => {
-  return new Promise((resolve, reject) => {
-    const request = new SetColorTemperatureRequest();
-    request.setTemperature(temperature);
-    client.setColorTemperature(request, {}, (err, response) => {
-      if (err) {
-        console.error('Error setting lightbulb color temperature:', err);
-        reject(err);
-      } else {
-        const state = {
-          isOn: response.getIsOn(),
-          brightness: response.getBrightness(),
-          colorTemperature: response.getColorTemperature()
-        };
-        resolve(state);
-      }
-    });
-  });
-}; 
+export const setColorTemperature = async (temperature) => {
+  const request = new SetColorTemperatureRequest().setTemperature(temperature);
+  const response = await client.setColorTemperature(request);
+  return responseToStateObject(response);
+};
